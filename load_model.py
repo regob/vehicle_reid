2529b324f934276ca147707db9712e61f0fd7e54
regob <rego97@gmail.com>
2022-05-08 11:22:18 +0200

    relative imports instead of absolutes

--- load_model.py
+++ load_model.py
@@ -2,8 +2,14 @@ import os
 import torch
 from torch import nn
 import yaml
+import sys
 
-from reid.vehicle_reid.model import ft_net, ft_net_dense, ft_net_hr, ft_net_swin, ft_net_efficient, ft_net_NAS, PCB
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from model import ft_net, ft_net_dense, ft_net_hr, ft_net_swin, ft_net_efficient, ft_net_NAS, PCB
+
+sys.path.remove(os.path.dirname(SCRIPT_DIR))
 
 
 def load_weights(model, ckpt_path):
@@ -46,24 +52,24 @@ def load_model(n_classes, kind="resnet", ckpt=None, remove_classifier=False, **k
 
 def load_model_from_opts(opts_file, ckpt=None, return_feature=False, remove_classifier=False):
     with open(opts_file, "r") as stream:
-        opts = yaml.load(stream, Loader=yaml.FullLoader)
-    n_classes = opts["nclasses"]
-    droprate = opts["droprate"]
-    stride = opts["stride"]
-    linear_num = opts["linear_num"]
-    model_subtype = "default" if "model_subtype" not in opts else opts["model_subtype"]
+        opts=yaml.load(stream, Loader=yaml.FullLoader)
+    n_classes=opts["nclasses"]
+    droprate=opts["droprate"]
+    stride=opts["stride"]
+    linear_num=opts["linear_num"]
+    model_subtype="default" if "model_subtype" not in opts else opts["model_subtype"]
 
     if opts["use_dense"]:
-        model = create_model(n_classes, "densenet", droprate=droprate, circle=return_feature,
+        model=create_model(n_classes, "densenet", droprate=droprate, circle=return_feature,
                              linear_num=linear_num)
     elif opts["use_efficient"]:
-        model = create_model(n_classes, "efficientnet", droprate=droprate,
+        model=create_model(n_classes, "efficientnet", droprate=droprate,
                              circle=return_feature, linear_num=linear_num, model_subtype=model_subtype)
     elif opts["use_NAS"]:
-        model = create_model(n_classes, "NAS", droprate=droprate,
+        model=create_model(n_classes, "NAS", droprate=droprate,
                              linear_num=linear_num)
     elif opts["PCB"]:
-        model = create_model(n_classes, "PCB")
+        model=create_model(n_classes, "PCB")
     elif opts["use_hr"]:
         model = create_model(n_classes, "hr", droprate=droprate, circle=return_feature,
                              linear_num=linear_num)
